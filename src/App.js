cb8463f368812c23d3529b76ffaeea13612d44c6
rendnil <andrew.lindner.15@gmail.com>
2018-10-31 17:26:38 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
+          
             Learn React
           </a>
         </header>
