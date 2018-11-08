3134ce272ff9c8d58bbc0f0d0b1ed78ee7bcdf7d
rendnil <andrew.lindner.15@gmail.com>
2018-11-08 15:34:48 -0500

    updated news page to receive crypto api

--- src/components/NewsPageListRow.js
+++ src/components/NewsPageListRow.js
@@ -6,13 +6,16 @@ import {Segment, List, Image} from "semantic-ui-react"
  const NewsPageListRow = ({article}) =>{
 
     return(
+
         <List.Item>
-          <Segment color="green">
-            <List.Header>{article.headline}</List.Header>
+          <Segment >
+            <List.Header>
+            <Image avatar src={article.imageurl}  />{article.title}</List.Header>
+
             <List.Content>
 
               <List.Description>
-                {article.summary}
+                {article.body}
               </List.Description>
               <List.Header>
                 Source: {article.source}
