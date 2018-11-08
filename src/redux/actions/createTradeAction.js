86d325948c08602fb9bcedf0584371104cc7e785
rendnil <andrew.lindner.15@gmail.com>
2018-11-07 21:05:44 -0500

    updated create trade action

--- src/redux/actions/createTradeAction.js
+++ src/redux/actions/createTradeAction.js
@@ -8,10 +8,12 @@ export function createNewTrade(userId, symbol, buy, price, quantity){
 
     dispatch({type: "about to create a new trade"})
 
+
     return createTradeAdapter.postNewTrade(userId, symbol, buy, price, quantity)
 
      .then(data => {
        console.log("this what we get back",data)
+       
       dispatch({type: "CREATE_NEW_TRADE",
 
       payload: data})
