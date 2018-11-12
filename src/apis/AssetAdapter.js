7707aecf2ed744e83e02f7b2023d36d03f63c57b
rendnil <andrew.lindner.15@gmail.com>
2018-11-12 17:23:09 -0500

    remove deprecated adapters

--- src/apis/AssetAdapter.js
+++
@@ -1,9 +0,0 @@
-const ASSET_API_ENDPOINT = "http://localhost:3001/api/v1/assets"
-
-export default class AssetAdapter{
-  static getAssets(){
-
-    return fetch(`${ASSET_API_ENDPOINT}`)
-    .then(r => r.json())
-  }
-}
--- src/apis/CoindeskAdapter.js
+++
@@ -1,9 +0,0 @@
-const COINDESK_API_ENDPOINT = "https://api.coindesk.com/v1/bpi/currentprice.json"
-
-export default class CoindeskAdapter{
-  static getBitcoin(){
-
-    return fetch(`${COINDESK_API_ENDPOINT}`)
-    .then(r => r.json())
-  }
-}
