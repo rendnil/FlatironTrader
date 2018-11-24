b1fa66b3e0bf9688fa58c5d1d0519a9a1f7a5750
rendnil <andrew.lindner.15@gmail.com>
2018-11-23 20:49:14 -0500

    refactor date building to parser class

--- src/components/Portfolio/PortfolioTableRow.js
+++ src/components/Portfolio/PortfolioTableRow.js
@@ -1,59 +1,34 @@
 import React from "react"
-import {Container, Table} from "semantic-ui-react"
+import {Table} from "semantic-ui-react"
+import DateBuilder from "../../parsers/DateBuilder"
 
 
-const PortfolioTableRow = (props) =>{
-
+const PortfolioTableRow = ({trade}) =>{
 
   const determineBuyOrSell = (trade) =>{
     return trade.buy? "Buy" : "Sell"
   }
 
-   const dateFormat = (rawDate) => {
-    let fullDate = new Date(rawDate)
-
-    return `${fullDate.getMonth()+1}/${fullDate.getDate()}/${fullDate.getFullYear()}`
+  const dateFormat = (rawDate) => {
+    return DateBuilder.dateFormat(Date.parse(rawDate))
   }
 
-    const timeFormat = (rawDate) =>{
-      let fullDate = new Date(rawDate)
-      let hours
-      let minutes
-      let seconds
-
-      if (fullDate.getHours() < 10){
-        hours = "0"+fullDate.getHours()
-      }else{
-        hours = fullDate.getHours()
-      }
-      if (fullDate.getMinutes() < 10){
-        minutes= "0"+fullDate.getMinutes()
-      }else{
-        minutes = fullDate.getMinutes()
-      }
-      if (fullDate.getSeconds() < 10){
-        seconds = "0"+fullDate.getSeconds()
-      }else{
-        seconds = fullDate.getSeconds()
-      }
-
-      return `${hours}:${minutes}:${seconds}`
-    }
+  const timeFormat = (rawDate) =>{
+    return DateBuilder.timeFormat(Date.parse(rawDate))
+  }
 
       return(
-
         <Table.Row>
-        <Table.Cell>{dateFormat(Date.parse(props.trade.created_at))}</Table.Cell>
-        <Table.Cell>{timeFormat(Date.parse(props.trade.created_at))}</Table.Cell>
-          <Table.Cell>{props.trade.asset.symbol}</Table.Cell>
-          <Table.Cell>{props.trade.asset.name}</Table.Cell>
-          <Table.Cell>{determineBuyOrSell(props.trade)}</Table.Cell>
-          <Table.Cell>{props.trade.price.toLocaleString()}</Table.Cell>
-          <Table.Cell>{props.trade.quantity}</Table.Cell>
-          <Table.Cell>{props.trade.pnl.toLocaleString()}</Table.Cell>
-          </Table.Row>
+          <Table.Cell>{dateFormat(trade.created_at)}</Table.Cell>
+          <Table.Cell>{timeFormat(trade.created_at)}</Table.Cell>
+          <Table.Cell>{trade.asset.symbol}</Table.Cell>
+          <Table.Cell>{trade.asset.name}</Table.Cell>
+          <Table.Cell>{determineBuyOrSell(trade)}</Table.Cell>
+          <Table.Cell>{trade.price.toLocaleString()}</Table.Cell>
+          <Table.Cell>{trade.quantity}</Table.Cell>
+          <Table.Cell>{trade.pnl.toLocaleString()}</Table.Cell>
+        </Table.Row>
         )
-
   }
 
   export default PortfolioTableRow
--- src/parsers/DateBuilder.js
+++ src/parsers/DateBuilder.js
@@ -1,10 +1,36 @@
 
 export default class DateBuilder {
 
+  static dateFormat(rawDate){
+   let fullDate = new Date(rawDate)
 
+   return `${fullDate.getMonth()+1}/${fullDate.getDate()}/${fullDate.getFullYear()}`
+ }
 
+   static timeFormat(rawDate){
+     let fullDate = new Date(rawDate)
+     let hours
+     let minutes
+     let seconds
 
+     if (fullDate.getHours() < 10){
+       hours = "0"+fullDate.getHours()
+     }else{
+       hours = fullDate.getHours()
+     }
+     if (fullDate.getMinutes() < 10){
+       minutes= "0"+fullDate.getMinutes()
+     }else{
+       minutes = fullDate.getMinutes()
+     }
+     if (fullDate.getSeconds() < 10){
+       seconds = "0"+fullDate.getSeconds()
+     }else{
+       seconds = fullDate.getSeconds()
+     }
 
-
+     return `${hours}:${minutes}:${seconds}`
+   }
+  
 
 } //end class
